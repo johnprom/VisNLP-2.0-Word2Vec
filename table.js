86fb17a8ca5aefea7f44e4cf5c10ea7d21afcc8c
aadhyaputtur <putturaadhya@gmail.com>
2022-07-09 13:36:04 -0400

    Js file to decrease repetive code

---
+++ table.js
@@ -0,0 +1,10 @@
+fetch('/backend/word2vec/matrix.json')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
